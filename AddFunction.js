b0a4d4f548f4d5dff044e152abf1405ad50a7d39
Boucheraki Wissam <ow_boucheraki@esi.dz>
2025-12-20 14:51:39 +0100

    why

--- AddFunction.js
+++ AddFunction.js
@@ -3,7 +3,7 @@ let b=7;
 let c=9;
 
 console.log("The product of a and b is: " + (a * b));
-console.log("The product of a and b is: " + (a * C));
+console.log("The product of a and b is: " + (a * c));
 
 
 //>>>>>NEW THINGS
